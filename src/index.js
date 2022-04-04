b7c8fa34b605829b5621a0c1e8bd62ea1e537a3f
Benjamin <bboksner@gmail.com>
2022-04-04 01:13:04 -0400

    initial fetch request

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-//load dom content
+//load dom content (event 1/3)
 document.addEventListener('DOMContentLoaded',()=>{
     //confirmation
 console.log('dom content loaded');
-//dom elements
+//dom elements 
+//var names correspond to dom element ids
 const location = document.getElementById('location');
 const cf = document.getElementById('cf');
 const conditionImg = document.getElementById('conditionImg');
@@ -10,6 +11,22 @@ const temp = document.getElementById('temp');
 const condition = document.getElementById('condition');
 const locationQuery = document.getElementById('locationQuery');
 const locationSubmit = document.getElementById('locationSubmit');
+//fetch params
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+        'X-RapidAPI-Key': '5e519ad7camsh9d194aefdd5d6e5p1bd575jsncd523e2ed36e'
+    }
+};
 //input listeners
-
+//(event 2/3)
+locationSubmit.addEventListener('click',()=>{
+   
+    
+    fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${locationQuery.value}`, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+})
 })
