c708c262a98895a2971ac3f4d4c8002b85d208c7
Benjamin <bboksner@gmail.com>
2022-04-04 00:51:26 -0400

    setting up js file and stored dom elements in variables

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//load dom content
+document.addEventListener('DOMContentLoaded',()=>{
+    //confirmation
+console.log('dom content loaded');
+//dom elements
+const location = document.getElementById('location');
+const cf = document.getElementById('cf');
+const conditionImg = document.getElementById('conditionImg');
+const temp = document.getElementById('temp');
+const condition = document.getElementById('condition');
+})
